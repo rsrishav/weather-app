d9d715dcd11031e6a3f1bb538da12a656a671345
Rishav Sharma <rsrishav0@gmail.com>
2021-08-17 16:53:14 +0530

    Heroku deployment preparation.

--- weather_app/main/views.py
+++ weather_app/main/views.py
@@ -9,27 +9,30 @@ def index(request):
         city = request.POST['city']
         # source contain JSON data from API
 
-        source = urllib.request.urlopen(f"http://api.openweathermap.org/data/2.5/weather?"
+        try:
+            source = urllib.request.urlopen(f"http://api.openweathermap.org/data/2.5/weather?"
                                         f"q={city}&appid={os.environ['WEATHER_API']}").read()
 
-        # converting JSON data to a dictionary
-        list_of_data = json.loads(source)
+            # converting JSON data to a dictionary
+            list_of_data = json.loads(source)
 
-        # data for variable list_of_data
-        data = {
-            "city_name": list_of_data['name'],
-            "country_code": (list_of_data['sys']['country']),
-            "coordinate": f"{(list_of_data['coord']['lon'])} {(list_of_data['coord']['lat'])}",
-            "temp": (list_of_data['main']['temp']),
-            "feels_like": list_of_data['main']['feels_like'],
-            "temp_max": list_of_data['main']['temp_max'],
-            "temp_min": list_of_data['main']['temp_min'],
-            "pressure": (list_of_data['main']['pressure']),
-            "humidity": (list_of_data['main']['humidity']),
-            "weather_condition": list_of_data['weather'][0]['main'],
-            "weather_condition_des": list_of_data['weather'][0]['description']
-        }
-        print(data)
+            # data for variable list_of_data
+            data = {
+                "city_name": list_of_data['name'],
+                "country_code": (list_of_data['sys']['country']),
+                "coordinate": f"{(list_of_data['coord']['lon'])} {(list_of_data['coord']['lat'])}",
+                "temp": f"{round(float(list_of_data['main']['temp'])-273.15, 2)}\u00B0C",
+                "feels_like": f"{round(float(list_of_data['main']['feels_like'])-273.15, 2)}\u00B0C",
+                "temp_max": f"{round(float(list_of_data['main']['temp_max'])-273.15, 2)}\u00B0C",
+                "temp_min": f"{round(float(list_of_data['main']['temp_min'])-273.15, 2)}\u00B0C",
+                "pressure": (list_of_data['main']['pressure']),
+                "humidity": f"{list_of_data['main']['humidity']}%",
+                "weather_condition": list_of_data['weather'][0]['main'],
+                "weather_condition_des": list_of_data['weather'][0]['description']
+            }
+            print(data)
+        except:
+            data = {"data_status": "not_found"}
     else:
         data = {}
     return render(request, "main/index.html", data)
--- weather_app/weather_app/settings.py
+++ weather_app/weather_app/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -23,14 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p!m1xmcof*9=bf6d%khto(-o=-(vr+!&)!r!wy5ro)6^ub+&o5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -69,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'weather_app.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -79,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -99,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -113,7 +111,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -123,3 +120,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# Activate Django-Heroku.
+django_heroku.settings(locals())
