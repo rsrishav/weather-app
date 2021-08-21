ba1a06ae6b88bbb9bdede94c31b2204e896e52e2
RishuRS <25101996rishu@gmail.com>
2021-08-21 13:43:25 +0530

    Uncommented.

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    # path('city/<str:search_city>/', views.get_cities),
+    path('city/<str:search_city>/', views.get_cities),
 ]
