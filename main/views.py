88af9d0fb47706c57e994e24b4c6edaffc636e40
RishuRS <25101996rishu@gmail.com>
2021-08-21 13:13:47 +0530

    City API commented.

--- main/views.py
+++ main/views.py
@@ -2,23 +2,22 @@ import os
 import json
 import urllib.request
 from django.shortcuts import render, HttpResponse
-import django
 import requests
 
-HEADERS = {
-        # 'dnt': '1',
-        # 'upgrade-insecure-requests': '1',
-        # 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-        # 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        # 'sec-fetch-site': 'same-origin',
-        # 'sec-fetch-mode': 'navigate',
-        # 'sec-fetch-user': '?1',
-        # 'sec-fetch-dest': 'document',
-        # 'referer': 'https://www.carwale.com/',
-        # 'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    }
-
-NO_OF_RECORDS = 5
+# HEADERS = {
+#         # 'dnt': '1',
+#         # 'upgrade-insecure-requests': '1',
+#         # 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+#         # 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+#         # 'sec-fetch-site': 'same-origin',
+#         # 'sec-fetch-mode': 'navigate',
+#         # 'sec-fetch-user': '?1',
+#         # 'sec-fetch-dest': 'document',
+#         # 'referer': 'https://www.carwale.com/',
+#         # 'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+#     }
+#
+# NO_OF_RECORDS = 5
 
 
 def weather_icons(weather):
@@ -31,12 +30,12 @@ def weather_icons(weather):
     return selector.get(weather.lower(), "fas fa-sun")
 
 
-def get_cities(request, search_city):
-    data = "[]"
-    if request.method == 'GET' and search_city not in ["null", None]:
-        # data = (requests.get(f"https://www.carwale.com/api/v2/autocomplete/city/?term={search_city}&record={NO_OF_RECORDS}&sourceId=1", headers=HEADERS).text)
-        print(data)
-    return HttpResponse(data)
+# def get_cities(request, search_city):
+#     data = "[]"
+#     if request.method == 'GET' and search_city not in ["null", None]:
+#         # data = (requests.get(f"https://www.carwale.com/api/v2/autocomplete/city/?term={search_city}&record={NO_OF_RECORDS}&sourceId=1", headers=HEADERS).text)
+#         print(data)
+#     return HttpResponse(data)
 
 
 def index(request):
