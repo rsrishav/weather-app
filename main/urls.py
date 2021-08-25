f3bee9f25c468e6399bda7d8138fbf12235d7bd5
Rishav Sharma <rsrishav0@gmail.com>
2021-08-25 10:33:59 +0530

    UI modifications.

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('v2', views.index2),
     path('city/<str:search_city>/', views.get_cities),
 ]
--- main/views.py
+++ main/views.py
@@ -94,3 +94,43 @@ def index(request):
     else:
         data = {}
     return render(request, "main/index.html", data)
+
+
+def index2(request):
+    if request.method == 'POST':
+        city = request.POST['city'].replace(" ", "")
+        # source contain JSON data from API
+
+        try:
+            source = urllib.request.urlopen(f"http://api.openweathermap.org/data/2.5/weather?q={city}"
+                                            f"&appid={os.environ['WEATHER_API']}").read()
+
+            # converting JSON data to a dictionary
+            list_of_data = json.loads(source)
+
+            # data for variable list_of_data
+            data = {
+                "city_name": list_of_data['name'],
+                "country_code": (list_of_data['sys']['country']),
+                "coordinate": f"{(list_of_data['coord']['lon'])} {(list_of_data['coord']['lat'])}",
+                "temp": f"{round(float(list_of_data['main']['temp'])-273.15, 1)}\u00B0C",
+                "feels_like": f"{round(float(list_of_data['main']['feels_like'])-273.15, 1)}\u00B0C",
+                "temp_max": f"{round(float(list_of_data['main']['temp_max'])-273.15, 1)}\u00B0C",
+                "temp_min": f"{round(float(list_of_data['main']['temp_min'])-273.15, 1)}\u00B0C",
+                "pressure": (list_of_data['main']['pressure']),
+                "humidity": f"{list_of_data['main']['humidity']}",
+                "weather_condition": list_of_data['weather'][0]['main'],
+                "weather_condition_des": list_of_data['weather'][0]['description'],
+                "wind": list_of_data['wind']['speed'],
+                "weather_icon": weather_icons(list_of_data["weather"][0]["main"]),
+                "weather_img": weather_img(list_of_data["weather"][0]["main"]),
+                "time": get_time(list_of_data["dt"])
+            }
+
+            print(data)
+        except Exception as e:
+            print(e)
+            data = {"data_status": "not_found"}
+    else:
+        data = {}
+    return render(request, "main/index2.html", data)
